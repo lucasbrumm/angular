e628190a0d64bbc96e1b41dbab48dfcaeebb5902
lucas.lugao <lucasbrumfurtado@hotmail.com>
2024-07-07 15:34:47 -0300

    feature: creating login component

--- spoleto/front/src/app/app.component.ts
+++ spoleto/front/src/app/app.component.ts
@@ -1,13 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FontAwesomeModule,
+    NgbAlertModule,
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'front';
+  demonio = 'demonio325';
+  faCoffee = faCoffee;
+  isOpen = false;
+  error = '';
+
+  toggleAlert() {
+    this.isOpen = !this.isOpen;
+  }
+
+  onSubmit(form: NgForm) {
+    this.error = 'ERRORRR';
+    console.log(form);
+  }
 }
---
+++ spoleto/front/src/app/login/login.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  error: string = '';
+
+  onSubmit(form: NgForm) {
+    this.error = 'ERRORRR';
+    console.log(form);
+  }
+}
